2bda414d3b4cf4ab5c934de108af88374f9e1441
Michael-MO <Michael-MO@users.noreply.github.com>
2019-03-19 14:23:45 +0100

    Create About component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <React.Fragment>
+      <h3>About</h3>
+      <p>This is the about page..</p>
+    </React.Fragment>
+  );
+};
+
+export default About;
