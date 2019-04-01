17b4c0c45af06d72ba450b916a2031eab138a922
Michael-MO <31982728+Michael-MO@users.noreply.github.com>
2019-04-01 23:19:20 +0200

    New Reducers for Async Data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,9 +9,35 @@ const slctMovieItemRdcr = (slctMovieItem = MOVIES[0], action) => {
   return slctMovieItem;
 };
 
-const getPostsRdcr = (state = [], action) => {
+const getPostsReducer = (state = [], action) => {
   switch (action.type) {
-    case "FETCH_POSTS":
+    case "GET_POSTS":
+      return action.payload.slice(0, 20);
+    default:
+      return state;
+  }
+};
+
+const selectedPostReducer = (state = null, action) => {
+  if (action.type === "SELECTED_POST") {
+    return action.payload;
+  }
+
+  return state;
+};
+
+const getCommentsReducer = (state = [], action) => {
+  switch (action.type) {
+    case "GET_COMMENTS":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const getUsersReducer = (state = [], action) => {
+  switch (action.type) {
+    case "GET_USERS":
       return action.payload;
     default:
       return state;
@@ -20,5 +46,8 @@ const getPostsRdcr = (state = [], action) => {
 
 export default combineReducers({
   slctMovieItem: slctMovieItemRdcr,
-  allPosts: getPostsRdcr
+  getPosts: getPostsReducer,
+  selectedPost: selectedPostReducer,
+  getComments: getCommentsReducer,
+  getUsers: getUsersReducer
 });
