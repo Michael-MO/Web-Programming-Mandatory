c6e6aeddf5a30b600ba10c5c37756384418f48a8
Michael-MO <31982728+Michael-MO@users.noreply.github.com>
2019-04-01 23:19:04 +0200

    New Action Creators for Async Data

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,10 +7,33 @@ export const slctMovieItem = movie => {
   };
 };
 
-export const fetchPosts = () => async dispatch => {
+export const getPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("posts");
   dispatch({
-    type: "FETCH_POSTS",
+    type: "GET_POSTS",
+    payload: response.data
+  });
+};
+
+export const selectedPost = post => {
+  return {
+    type: "SELECTED_POST",
+    payload: post
+  };
+};
+
+export const getComments = () => async dispatch => {
+  const response = await jsonPlaceholder.get("comments");
+  dispatch({
+    type: "GET_COMMENTS",
+    payload: response.data
+  });
+};
+
+export const getusers = () => async dispatch => {
+  const response = await jsonPlaceholder.get("users");
+  dispatch({
+    type: "GET_USERS",
     payload: response.data
   });
 };
