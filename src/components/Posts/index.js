9a735e09ba7993fdfe8db6405fbd0758465dff40
Michael-MO <Michael-MO@users.noreply.github.com>
2019-04-02 09:57:50 +0200

    Uncomment routing for the 'SinglePost' component

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import AllPosts from "./AllPosts";
+import SinglePost from "./SinglePost";
 import { Switch, Route } from "react-router-dom";
 
 const Posts = () => {
   return (
     <Switch>
       <Route exact path="/posts" component={AllPosts} />
-      {/* <Route exact path="/posts/:int" component={SinglePost} /> */}
+      <Route exact path="/posts/:int" component={SinglePost} />
     </Switch>
   );
 };
