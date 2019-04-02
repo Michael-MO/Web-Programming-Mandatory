a3d006b12a8bac97985878c9b8b73ff4b098f0b8
Michael-MO <Michael-MO@users.noreply.github.com>
2019-04-02 14:34:54 +0200

    Wrap the 'jsonplaceholder'-site in an anchor tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,17 @@ import React from "react";
 const Home = () => {
   return (
     <div className="jumbotron">
-      <h1 className="display-4">JSONPlaceholder</h1>
+      <h1 className="display-4 font-weight-bold">JSONPlaceholder</h1>
       <p className="lead">
         This Web App utilizes React, React-Redux, Redux, Redux-thunk, Axios
       </p>
       <hr className="my-4" />
       <p>
-        This mandatory project, uses 'https://jsonplaceholder.typicode.com/'s
-        api for fetching Users, Posts and Comments information.
+        This mandatory project, uses '
+        <a href="https://jsonplaceholder.typicode.com/" target="blank">
+          https://jsonplaceholder.typicode.com/
+        </a>
+        's api for fetching Users, Posts and Comments information.
       </p>
     </div>
   );
