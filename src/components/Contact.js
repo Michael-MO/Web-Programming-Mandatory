05228e3d62de1cbdba54f5d84ebfa8d29bc181b0
Michael-MO <31982728+Michael-MO@users.noreply.github.com>
2019-04-04 18:17:57 +0200

    Populate the "Contact" Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,55 @@ const Contact = () => {
   return (
     <React.Fragment>
       <h3>Contact</h3>
-      <p>This is the contacts page..</p>
+      <div className="form-group row">
+        <label
+          for="colFormLabel"
+          className="col-sm-2 offset-lg-3 col-form-label"
+        >
+          Name
+        </label>
+        <div className="col-lg-4">
+          <input type="text" className="form-control" id="colFormLabel" />
+        </div>
+      </div>
+      <div className="form-group row">
+        <label
+          for="colFormLabel"
+          className="col-sm-2 offset-lg-3 col-form-label"
+        >
+          Email
+        </label>
+        <div className="col-lg-4">
+          <input type="email" className="form-control" id="colFormLabel" />
+        </div>
+      </div>
+      <div className="form-group row">
+        <label
+          for="colFormLabel"
+          className="col-sm-2 offset-lg-3 col-form-label"
+        >
+          Subject
+        </label>
+        <div className="col-lg-4">
+          <input type="text" className="form-control" id="colFormLabel" />
+        </div>
+      </div>
+      <div className="form-group row">
+        <div className="col-lg-6 offset-lg-3">
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          />
+        </div>
+      </div>
+      <div className="form-group row">
+        <div className="col-sm-12 offset-lg-3">
+          <button type="submit" class="btn btn-primary mb-2">
+            Send
+          </button>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
