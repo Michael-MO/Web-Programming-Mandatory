8a9e4fde54004f2e666edd99e2cf948d6bc1ae31
Michael-MO <31982728+Michael-MO@users.noreply.github.com>
2019-04-04 18:18:23 +0200

    Missing closing tag for UL-element

--- src/components/Posts/SinglePost.js
+++ src/components/Posts/SinglePost.js
@@ -19,22 +19,23 @@ class SinglePost extends Component {
         <p className="mt-3">{this.props.selectedPost.body}</p>
         <hr />
         <h5 className="mb-5">Comments</h5>
-        <ul className="list-unstyled" />
-        {this.props.comments.map(comment => {
-          return (
-            <li key={comment.id} className="media mb-3 col-md-10">
-              <img
-                src="https://via.placeholder.com/75"
-                className="rounded-circle mr-4 d-none d-sm-block"
-                alt="..."
-              />
-              <div className="media-body">
-                <h5 className="mt-0 mb-1">{comment.name}</h5>
-                {comment.body}
-              </div>
-            </li>
-          );
-        })}
+        <ul className="list-unstyled">
+          {this.props.comments.map(comment => {
+            return (
+              <li key={comment.id} className="media mb-3 col-md-10">
+                <img
+                  src="https://via.placeholder.com/75"
+                  className="rounded-circle mr-4 d-none d-sm-block"
+                  alt="..."
+                />
+                <div className="media-body">
+                  <h5 className="mt-0 mb-1">{comment.name}</h5>
+                  {comment.body}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </React.Fragment>
     );
   }
