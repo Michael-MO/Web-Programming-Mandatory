5c102360960352231a50b7cd006bb2471b68e9a7
Michael-MO <Michael-MO@users.noreply.github.com>
2019-04-09 14:32:53 +0200

    This component does not need to know the selected post?

--- src/components/Posts/AllPosts.js
+++ src/components/Posts/AllPosts.js
@@ -5,7 +5,7 @@ import {
   getPostsAction,
   getUsersAction,
   getCommentsAction,
-  selectedPostAction
+  selectPostAction
 } from "../../actions";
 
 class AllPosts extends Component {
@@ -16,8 +16,8 @@ class AllPosts extends Component {
   }
 
   // I am uncertain of this approach.
-  // But currently don't see any other way, to create a relationship between two 'tables' of data.
-  mergeArrays() {
+  // But currently don't see any other way, to create a correlation between two 'tables' of data.
+  doCorrelationsArray() {
     let newArray = [];
 
     this.props.posts.forEach(post => {
@@ -35,49 +35,53 @@ class AllPosts extends Component {
     return newArray;
   }
 
+  renderTableData() {
+    return this.doCorrelationsArray().map(post => {
+      return (
+        <tr>
+          <td className="no-stretch">
+            <i className="far fa-comments posts-icon pl-2" />
+          </td>
+          <td>
+            <Link
+              key={post.id}
+              className="text-info font-weight-bold"
+              to={"/posts/" + post.id}
+              onClick={() => this.props.selectPostAction(post)}
+            >
+              {post.title}
+            </Link>
+            <br />
+            {post.body.slice(0, 40)}..
+          </td>
+          <td className="col-2 d-none d-md-table-cell">{post.userName}</td>
+          <td className="col-2 d-none d-md-table-cell">
+            {post.comments.length}
+          </td>
+        </tr>
+      );
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <h3>Posts</h3>
-        <table className="table table-striped mt-3 table-posts">
+        <h3 className="mb-4">Posts</h3>
+        <table className="table mt-3 table-posts">
           <thead>
             <tr>
               <th colSpan="2">
                 <h5>Title</h5>
               </th>
-              <th>
+              <th className="d-none d-md-table-cell">
                 <h5>Posted by</h5>
               </th>
-              <th>
+              <th className="d-none d-md-table-cell">
                 <h5>Comments</h5>
               </th>
             </tr>
           </thead>
-          <tbody>
-            {this.mergeArrays().map(post => {
-              return (
-                <tr>
-                  <td className="no-stretch">
-                    <i className="fas fa-file posts-icon pl-2" />
-                  </td>
-                  <td>
-                    <Link
-                      key={post.id}
-                      className="text-info font-weight-bold"
-                      to={"/posts/" + post.id}
-                      onClick={() => this.props.selectedPostAction(post)}
-                    >
-                      {post.title}
-                    </Link>
-                    <br />
-                    {post.body.slice(0, 40)}..
-                  </td>
-                  <td className="col-2">{post.userName}</td>
-                  <td className="col-2">{post.comments.length}</td>
-                </tr>
-              );
-            })}
-          </tbody>
+          <tbody>{this.renderTableData()}</tbody>
         </table>
       </React.Fragment>
     );
@@ -88,12 +92,11 @@ const mapStateToProps = state => {
   return {
     posts: state.getPosts,
     users: state.getUsers,
-    comments: state.getComments,
-    selectedPost: state.selectedPost
+    comments: state.getComments
   };
 };
 
 export default connect(
   mapStateToProps,
-  { getPostsAction, getUsersAction, getCommentsAction, selectedPostAction }
+  { getPostsAction, getUsersAction, getCommentsAction, selectPostAction }
 )(AllPosts);
