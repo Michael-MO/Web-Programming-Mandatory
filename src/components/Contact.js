3842dd3b96fec7a8fe78cfd6df6a9fafd5da9e80
Michael-MO <Michael-MO@users.noreply.github.com>
2019-04-09 14:38:27 +0200

    Cleanup some small warnings

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,52 +3,48 @@ import React from "react";
 const Contact = () => {
   return (
     <React.Fragment>
-      <h3>Contact</h3>
+      <h3 className="mb-4">Contact</h3>
       <div className="form-group row">
         <label
-          for="colFormLabel"
+          htmlFor="formUsername"
           className="col-sm-1 offset-lg-3 col-form-label"
         >
           Name
         </label>
         <div className="col-lg-5">
-          <input type="text" className="form-control" id="colFormLabel" />
+          <input type="text" className="form-control" id="formUsername" />
         </div>
       </div>
       <div className="form-group row">
         <label
-          for="colFormLabel"
+          htmlFor="formEmail"
           className="col-sm-1 offset-lg-3 col-form-label"
         >
           Email
         </label>
         <div className="col-lg-5">
-          <input type="email" className="form-control" id="colFormLabel" />
+          <input type="email" className="form-control" id="formEmail" />
         </div>
       </div>
       <div className="form-group row">
         <label
-          for="colFormLabel"
+          htmlFor="formSubject"
           className="col-sm-1 offset-lg-3 col-form-label"
         >
           Subject
         </label>
         <div className="col-lg-5">
-          <input type="text" className="form-control" id="colFormLabel" />
+          <input type="text" className="form-control" id="formSubject" />
         </div>
       </div>
       <div className="form-group row">
         <div className="col-lg-6 offset-lg-3">
-          <textarea
-            className="form-control"
-            id="exampleFormControlTextarea1"
-            rows="3"
-          />
+          <textarea className="form-control" rows="3" />
         </div>
       </div>
       <div className="form-group row">
         <div className="col-sm-12 offset-lg-3">
-          <button type="submit" class="btn btn-primary">
+          <button type="submit" className="btn btn-primary">
             Send
           </button>
         </div>
