c8be27a9ee4f7fc8d2ba72edd8dca5aea8b4f6cc
Michael-MO <Michael-MO@users.noreply.github.com>
2019-04-09 14:35:14 +0200

    Make this component more responsive to different screensizes

--- src/components/movies/index.js
+++ src/components/movies/index.js
@@ -8,10 +8,10 @@ const Movies = () => {
     <React.Fragment>
       <h3 className="mb-4">Movies</h3>
       <div className="row">
-        <div className="col-3">
+        <div className="col-lg-3 col-md-5">
           <MoviesList />
         </div>
-        <div className="col-9">
+        <div className="col-lg-9 col-md-7">
           <Switch>
             <Route path="/movies/:string" component={MoviesDetails} />
           </Switch>
